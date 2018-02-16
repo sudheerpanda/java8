e229d9ed2c4fda789d6f89d0145e002c2ec103de
sudheerpanda <sudheer.panda@imaginea.com>
2018-02-16 12:46:52 +0530

    Consumer interface example

---
+++ src/main/java/com/java8/lambdas/ConsumerInterfaceEx.java
@@ -0,0 +1,17 @@
+package com.java8.lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerInterfaceEx {
+    public static void main(String[] args) {
+        List<Integer> numbers= Arrays.asList(1,2,6,3,4,5,6,7,8,9,0);
+        Consumer consumer=new Consumer<Integer>() {
+            public void accept(Integer t) {
+                System.out.println(t);
+            }
+        };
+        numbers.forEach(consumer);
+    }
+}
